52ae91bdd66a47236943618d64fb02fff360c9b8
Aki Rehn <aki.rehn@helsinki.fi>
2018-11-18 20:18:20 +0200

    Switchin pw hash field in SQLAlchemy to DB.Binary
    Hopefully this fixes the weird password hash problems. Also remove
    previous utf-8 encode hacks in a hope that they are not needed after
    all.

--- application/auth/views.py
+++ application/auth/views.py
@@ -16,7 +16,7 @@ def auth_login():
     if not user:
         return render_template('auth/login_form.html', form=form, error='No such username or password')
 
-    if not bcrypt.check_password_hash(user.password.encode('utf-8'), form.password.data.encode('utf-8')):
+    if not bcrypt.check_password_hash(user.password, form.password.data):
         return render_template('auth/login_form.html', form=form, error='No such username or password')
 
     login_user(user)
@@ -37,7 +37,7 @@ def auth_register():
     if form.password.data != form.password_confirm.data:
         return render_template('auth/register_form.html', form=form, error='Passwords do not match')
 
-    pw_hash = bcrypt.generate_password_hash(form.password.data.encode('utf-8'))
+    pw_hash = bcrypt.generate_password_hash(form.password.data)
     new_user = User(form.fullname.data, form.username.data, pw_hash)
     db.session().add(new_user)
     db.session().commit()
