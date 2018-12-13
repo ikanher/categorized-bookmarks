d3d88d9d5cba939c5ea11e564f312ca5405491a4
Aki Rehn <aki.rehn@helsinki.fi>
2018-12-13 13:33:18 +0200

    Replace unnecessary jQuery with plain Javascript

--- application/static/onload.js
+++ application/static/onload.js
@@ -1,5 +1,5 @@
-const activateTooltips = () => $('[data-toggle="tooltip"]').tooltip()
-const dateToFooter = () => { let now = new Date(); $('#now').text(now) }
+const dateToFooter = () => document.querySelector('#now').innerText = new Date()
+const activateTooltips = () => document.querySelectorAll('[data-toggle="tooltip"]').forEach(link => $(link).tooltip())
 
 document.addEventListener("DOMContentLoaded", function() {
     activateTooltips()
