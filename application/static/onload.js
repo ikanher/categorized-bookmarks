152f8940097df98570fb03398aec0a7ebc89228b
Aki Rehn <aki.rehn@helsinki.fi>
2018-12-13 21:33:45 +0200

    Move Javascript to onload.js

--- application/static/onload.js
+++ application/static/onload.js
@@ -5,3 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
     activateTooltips()
     dateToFooter()
 });
+
+const filterBookmarks = () => document.querySelectorAll(".row.bookmark div a.bookmark-link")
+    .forEach(link => link.innerHTML.toLowerCase().includes(document.querySelector('#filter_field').value.toLowerCase())
+        ? link.parentNode.parentNode.parentNode.classList.remove('d-none')
+        : link.parentNode.parentNode.parentNode.classList.add('d-none'))
+
+const filterCategories = () => document.querySelectorAll('.category-card .card-header')
+    .forEach(link => link.innerText.toLowerCase().includes(document.querySelector('#filter_field').value.toLowerCase())
+        ? link.parentNode.classList.remove('d-none')
+        : link.parentNode.classList.add('d-none'))
